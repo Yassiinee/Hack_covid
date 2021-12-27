29fa6c70692f18ccf85dfa5258df3ed363745e30
Yassine Zakhama <31131273+Yassiinee@users.noreply.github.com>
2021-12-27 22:49:52 +0100

    mongodb link

--- INNOVER CONTRE COVID19/mongoose.js
+++ INNOVER CONTRE COVID19/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://admin:YQ50hi8HNsDZDdIb@cluster0-xlhan.mongodb.net/INNOVER_CONTRE_COVID19?retryWrites=true&w=majority",
+  "mongodb+srv://admin:........@..............mongodb.net/INNOVER_CONTRE_COVID19?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
